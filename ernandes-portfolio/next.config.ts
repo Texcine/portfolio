18e65904aecef465b7942c951c3948208ef7e205
Texcine <ernandessouzadias115@Gmail.com>
2025-08-19 09:57:15 -0300

    Final Fix: Remove basePath para deploy na raiz do site

--- ernandes-portfolio/next.config.ts
+++ ernandes-portfolio/next.config.ts
@@ -1,16 +1,10 @@
 import type { NextConfig } from "next";
 
-// Define o nome do seu repositório
-const repo = 'portfolio';
-const assetPrefix = `/${repo}/`;
-const basePath = `/${repo}`;
-
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  assetPrefix: assetPrefix,
-  basePath: basePath,
+
 };
 
 export default nextConfig;
