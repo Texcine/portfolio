ae94a39a5d4f35ff22ea8cc3ee5181330a826f48
Texcine <ernandessouzadias115@Gmail.com>
2025-08-19 09:41:52 -0300

    Fix: Configura basePath e assetPrefix para o GitHub Pages

--- ernandes-portfolio/next.config.ts
+++ ernandes-portfolio/next.config.ts
@@ -1,8 +1,22 @@
 import type { NextConfig } from "next";
 
+// Define o nome do seu repositório
+const repo = 'portfolio';
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}`;
+
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+
+  images: {
+    // ESSENCIAL para o next/image funcionar corretamente na exportação estática
+    loader: 'custom',
+    loaderFile: './src/components/Image/loader.ts',
+  },
 };
 
 export default nextConfig;
